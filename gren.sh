a126d93275cb9c9ff9434ddf2d52c0d1e7761bf9
Travis CI <travis@travis-ci.org>
2018-06-11 15:03:20 +0530

    Fix gren.sh access token #2

--- gren.sh
+++ gren.sh
@@ -1,5 +1,5 @@
 # Create release notes based on issues and update changelog
 
-gren release --tags=$tagName --override --token=${GREN_TOKEN}
+gren release --tags=$tagName --override --token=$GREN_TOKEN
 
-gren changelog --token=${GREN_TOKEN}
+gren changelog --token=$GREN_TOKEN
